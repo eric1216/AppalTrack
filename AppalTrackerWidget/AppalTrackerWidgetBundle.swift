acb00836250d6ebc58643715bfb03529254401c3
Eric Hendershot <ericolebusiness@gmail.com>
2026-03-21 17:26:37 -0400

    init

---
+++ AppalTrackerWidget/AppalTrackerWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  AppalTrackerWidgetBundle.swift
+//  AppalTrackerWidget
+//
+//  Created by Eric Hendershot on 3/21/26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct AppalTrackerWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        AppalTrackerWidget()
+    }
+}
