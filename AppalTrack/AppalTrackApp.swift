82f6af721780fb4a993df04c7c18f4307a404136
Eric Hendershot <ericolebusiness@gmail.com>
2026-03-21 17:19:03 -0400

    Initial Commit

---
+++ AppalTrack/AppalTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppalTrackApp.swift
+//  AppalTrack
+//
+//  Created by Eric Hendershot on 3/21/26.
+//
+
+import SwiftUI
+
+@main
+struct AppalTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
